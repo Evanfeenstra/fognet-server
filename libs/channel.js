30fc3708db01fbdc5c8dd3dc3e34bfd4b330cdf1
Lewis Freiberg <lewi@2pk.in>
2017-09-30 23:34:16 +1000

    Update to new flash.lib.js

--- libs/channel.js
+++ libs/channel.js
@@ -1,101 +1,109 @@
-const subseed = require("iota.crypto.js").signing.subseed;
-const converter = require("iota.crypto.js").converter;
-const storage = require("./storage"); 
-const Flash = require("iota.flash.js");
-const multisig = Flash.multisig;
-const transfer = Flash.transfer;
-
+const subseed = require("iota.crypto.js").signing.subseed
+const converter = require("iota.crypto.js").converter
+const storage = require("./storage")
+const multisig = require("iota.flash.js").multisig
+const transfer = require("iota.flash.js").transfer
 
 function getSubseed(seed, callback) {
-  storage.incr('index', (err, index) => {
-    const subseedTrits = subseed(converter.trits(seed), index);
+  storage.incr("index", (err, index) => {
+    const subseedTrits = subseed(converter.trits(seed), index)
     //console.log(subseedTrits);
-    callback(err, err ? null : converter.trytes(subseedTrits));
-  });
+    callback(err, err ? null : converter.trytes(subseedTrits))
+  })
 }
 
 function getDigest(seed, index, security) {
-  return multisig.getDigest(seed, index, security);
+  return multisig.getDigest(seed, index, security)
 }
 
 function getNewDigest(id, callback) {
-  const state = storage.get('channel_' + id, (err, channel) => {
+  const state = storage.get("channel_" + id, (err, channel) => {
     if (err) {
-      callback(err);
+      callback(err)
+    } else {
+      callback(
+        null,
+        multisig.getDigest(
+          channel.seed,
+          channel.flash.index,
+          channel.flash.security
+        )
+      )
     }
-    else {
-      callback(null, multisig.getDigest(channel.seed, channel.flash.index, channel.flash.security));
-    }
-  });
+  })
 }
 
 function getAddress(digests) {
-  return multisig.composeAddress(digests);
+  return multisig.composeAddress(digests)
 }
 
 function processTransfer(id, item, bundles, callback) {
-  storage.get('channel_' + id, (err, channel) => {
+  storage.get("channel_" + id, (err, channel) => {
     if (err) {
-      callback(err);
-      return;
+      callback(err)
+      return
     }
     try {
-      const state = channel.flash.state;
-      const addressesToCheck = transfer.getDiff(
-        state.root, 
-        state.remainderAddress, 
-        state.transfers, 
-        bundles).filter(tx => tx.value >= item.value).map(tx => tx.address);
-      validateOutputs(addressesToCheck, (err, valid) => {
-        if (err) {
-          callback(err);
-          return;
-        }
-        if (!valid) {
-          callback(null, false);
-          return;
-        }
-        const flashState = channel.flash.state;
+      // const addressesToCheck = transfer
+      //   .getDiff(state.root, state.remainderAddress, state.transfers, bundles)
+      //   .filter(tx => tx.value >= item.value)
+      //   .map(tx => tx.address)
+      // validateOutputs(addressesToCheck, (err, valid) => {
+      //   if (err) {
+      //     callback(err)
+      //     return
+      //   }
+      //   if (!valid) {
+      //     callback(null, false)
+      //     return
+      //   }
+      const flashState = channel.flash
+      const signatures = transfer.sign(
+        flashState.root,
+        channel.seed,
+        bundles
+      )
 
-        const signedBundles = transfer.sign(channel.flash.state.root, channel.seed, bundles);
-        transfer.applyTransfers(
-          flashState.root, 
-          flashState.deposit, 
-          flashState.stakes, 
-          flashState.outputs, 
-          flashState.remainderAddress, 
-          flashState.transfers, 
-          signedBundles);
-        storage.set('channel_' + id, channel, (err, res) => {
-          if (err) {
-            callback(err);
-          }
-          else {
-            callback(null, signedBundles);
-          }
-        });
-      });
-    } 
-    catch (err) {
-      console.log(err);
-      callback(null, false); 
+      let signedBundles = transfer.appliedSignatures(bundles, signatures)
+
+      transfer.applyTransfers(
+        flashState.root,
+        flashState.deposit,
+        flashState.outputs,
+        flashState.remainderAddress,
+        flashState.transfers,
+        signedBundles,
+        item.value == 0 ? true : false
+      )
+      storage.set("channel_" + id, channel, (err, res) => {
+        if (err) {
+          callback(err)
+        } else {
+          callback(null, signedBundles)
+        }
+      })
+      //     })
+    } catch (err) {
+      console.log(err)
+      callback(null, false)
     }
-  });
+  })
 }
 
 function validateOutputs(addresses, callback) {
   // TODO: replace with storage
-  const whitelist = ["TRPSU9DSNROHLCPIXBXGDXPOLKPUOYZZBZJCEILRJNSIFZASLPKHCIDIDBRCJHASMENZMTICJMBZRANKM",'AGGXW9LBTUORZLBTIQCPUOCCHIJJE9EFXYOHIIJMXRALPUWWJGRGTTAUCJJXVMYETNQVTTYDDEVCJRPZT'];
+  const whitelist = [
+    "TRPSU9DSNROHLCPIXBXGDXPOLKPUOYZZBZJCEILRJNSIFZASLPKHCIDIDBRCJHASMENZMTICJMBZRANKM",
+    "AGGXW9LBTUORZLBTIQCPUOCCHIJJE9EFXYOHIIJMXRALPUWWJGRGTTAUCJJXVMYETNQVTTYDDEVCJRPZT"
+  ]
   // callback(null, !!addresses.filter((a) => whitelist.indexOf(a) !== -1).length);
-    callback(null, true);
-
+  callback(null, true)
 }
 
 module.exports = {
-  'getSubseed'     : getSubseed,
-  'getDigest'      : getDigest,
-  'getNewDigest'   : getNewDigest,
-  'getAddress'     : getAddress,
-  'processTransfer': processTransfer
+  getSubseed: getSubseed,
+  getDigest: getDigest,
+  getNewDigest: getNewDigest,
+  getAddress: getAddress,
+  processTransfer: processTransfer
 }
-
